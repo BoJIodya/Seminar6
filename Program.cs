182c72f1cf1d15d17c6517417735e070326bc7f1
Vladimir Loginov <BoJIodya@yandex.ru>
2022-05-13 09:52:16 +0300

    заполение и печать двумерного массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void RandomArray(int[,] array) //заполнение массива рандомно
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array) //печать массива
+{
+for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write ($" {array[i,j]}");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] array = new int[3, 3];
+RandomArray(array);
+PrintArray(array);
+Console.WriteLine(); // пустая строка для отделения напечатанного массива
+
