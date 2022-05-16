aaadc3d937dce0a46ca1373d020b183684629121
Vladimir Loginov <BoJIodya@yandex.ru>
2022-05-16 16:50:10 +0300

    отладка цикла счета

--- Seminar6/Задача1-1/Program.cs
+++ Seminar6/Задача1-1/Program.cs
@@ -22,34 +22,30 @@ void PrintArray(int[,] array) //печать массива
 
 }
 
-int Chastot(int[,] array) //перебор всех элементов массива и подсчет подобных
+int[,] FreqDictionary(int[,] array) //перебор всех элементов массива и подсчет подобных
 {
-    //int[] array1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    int c = 0;
     double ind = array.GetLength(0) * array.GetLongLength(1);
-    Console.WriteLine($"ind = {ind}");
     int[,] result = new int[2, (int)ind]; //в первой строке запись значения, во второй частота повтора
+    int count = 0; //переменная для ввода в новый массив данных
 
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            foreach (int element in array)
-                if (array[i, j] == element)
-                {
-                    result[0, c] = array[i, j];
-                    result[1, c] += 1;
-                    c += 1;
+           // int number = array[i, j];
+            foreach (int number in array)
+            if (number == array[i, j])
+            {
+                result[0, count] = number;
+                result[1, count] += 1;
+                Console.WriteLine($"number = {number}");
+                Console.WriteLine($"count = {count}");
+                count++;
+            }
 
-                    //double index = (array.GetLength(0) * array.GetLongLength(1) - 1) + c;
-                    //Console.WriteLine($"index = {index}");
-                    //result[c] = element; //запись эл-та
-                    //result[(int)index] += 1; //счет записи во второй половине массива
-                }
-            //Console.WriteLine(count);
         }
     }
-    return result[2, (int)ind];
+    return result;
 
 }
 
@@ -58,8 +54,8 @@ int[,] array = new int[3, 3];
 RandomArray(array);
 PrintArray(array);
 Console.WriteLine(); // пустая строка для отделения напечатанного массива
-//int count = Chastot(array);
+//int count = FreqDictionary(array);
 double j = array.GetLength(0) * array.GetLongLength(1);
 int[,] result = new int[2, (int)j];
-Chastot(array);
+result = FreqDictionary(array);
 PrintArray(result); // печать массива результата
